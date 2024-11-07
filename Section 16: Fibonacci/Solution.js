b74a32f6a23e997a5b54b636315261727e17d854
sohantalukder <mdtalukder.sohan@gmail.com>
2024-11-07 07:11:14 +0600

    solve fibonacci problem using recursive

--- Section 16: Fibonacci/Solution.js
+++ Section 16: Fibonacci/Solution.js
@@ -1,10 +1,16 @@
-function fib(n){
-    if(n <= 0) return 0;
-    if(n === 1) return 1;
-   const b=[0, 1];
-   for(let i=2;i<=n;i++){
-       b.push(b[i-1]+b[i-2]);
-   }
-   return b[n];
+// function fib(n){
+//     if(n <= 0) return 0;
+//     if(n === 1) return 1;
+//    const b=[0, 1];
+//    for(let i=2;i<=n;i++){
+//        b.push(b[i-1]+b[i-2]);
+//    }
+//    return b[n];
+// }
+
+// recursive solution
+function fib(n) {
+  if(n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
 }
 console.log(fib(10)); // 55
